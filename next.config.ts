a149b1c5352140cacdf6bb16f9e99b0232593cdc
Risiidhan <rkishon4@gmail.com>
2024-12-25 11:32:19 +0400

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: 'dist',
   env: {
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY, // Access the API key
   },
