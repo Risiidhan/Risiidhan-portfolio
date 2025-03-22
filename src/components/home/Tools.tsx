da8681d493e425d732215c2bfdf99babef5edf8c
Risiidhan <rkishon4@gmail.com>
2025-03-22 16:35:38 +0400

    .

--- src/components/home/Tools.tsx
+++ src/components/home/Tools.tsx
@@ -4,8 +4,37 @@ import { motion } from 'framer-motion'
 import React from 'react'
 import { fadeIn } from '../partials/framer-motion'
 import Marquee from 'react-fast-marquee'
+import Image from "next/image"
 
 const Tools = () => {
+
+    const stacks1 = [
+        "https://angular.io/assets/images/logos/angular/angular.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-plain-wordmark.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg",
+    ]
+
+    const stacks2 = [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg",
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original.svg",
+    ]
+    const stacks3 = [
+        "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+        "https://www.chartjs.org/media/logo-title.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    ]
+
+
     return (
         <div className=''>
             <motion.div
@@ -16,59 +45,46 @@ const Tools = () => {
                 viewport={{ once: false, amount: 0.7 }}
                 className='md:col-span-1 col-span-3 md:order-2 mt-[-20%] order-1 grid place-items-center'>
                 <span className='text-2xl sm:text-[50px] mb-10'>
-                    Technologies
+
                 </span>
+
+                <div className="max-w-7xl mx-auto pt-20 pb-10 px-4 md:px-8 lg:px-10">
+                    <h2 className="text-xl text-center md:text-3xl mb-4 text-black dark:text-white max-w-4xl">
+                        Technologies
+                    </h2>
+                    <p className="text-neutral-700 dark:text-neutral-300 md:text-lg max-w-sm">
+                        Languages and Frameworks
+                    </p>
+                </div>
             </motion.div>
-            <section className='mb-24'>
-                <motion.section
-                    variants={fadeIn("up", 0.1)}
-                    initial="hidden"
-                    whileInView={"show"}
-                    viewport={{ once: true, amount: 0.5 }}
-                    className=''>
-                    <div className='inner-glass-effect'>
+            <section className='mb-24 hidden md:block'>
+                {[stacks1, stacks2, stacks3].map((stack, index) => (
+                    <section key={index} className={`w-full flex ${index == 1 ? "justify-start" : "justify-center"} my-4`}>
+                        <div className='flex justify-between w-[90%]'>
+                            {stack.map((item, subIndex) => (
+                                <div key={subIndex} className={`w-fit border transition-all duration-100 ${subIndex%2== 0 ? "reverse-vertical-move-animation": "vertical-move-animation"} rounded-full
+                                 h-[100px] mx-2 flex justify-center aspect-square`}>
+                                    <Image src={item} width={50} height={50} className='h-auto aspect-square w-[50px]' alt="" />
+                                </div>
+                            ))}
+                        </div>
+                    </section>
+                ))}
+            </section>
 
-
-                        <h1 className='text-center text-[16.8px] sm:text-[20px] pl-0 md:pl-6 pt-3 md:text-md mb-4 tracking-wide'>
-                            Languages and Frameworks:</h1>
-                        <Marquee autoFill={true}>
-                            <div className='flex items-center space-x-8 py-3 md:py-6 ml-6'>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20 mx-2' href="https://angular.io" target="blank" rel="noreferrer"> <img src="https://angular.io/assets/images/logos/angular/angular.svg" alt="angular" /> </a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://nextjs.org/" target="blank" rel="noreferrer"> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" /></a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://reactjs.org/" target="blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" /> </a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://www.w3schools.com/cs/" target="blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="csharp" /> </a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://nodejs.org" target="blank" rel="noreferrer"> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-plain-wordmark.svg" /></a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://dotnet.microsoft.com/" target="blank" rel="noreferrer"> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg" /></a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://expressjs.com" target="blank" rel="noreferrer">
-                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" />
-                                </a>
-                            </div>
-                        </Marquee>
-                        <Marquee autoFill={true} direction='right'>
-                            <div className='flex items-center  space-x-8 py-3 md:py-6 ml-6'>
-                                <a className="w-9 md:w-16  2xl:w-20 h-9 md:h-16  2xl:h-20" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" /> </a>
-                                <a className="w-9 md:w-16  2xl:w-20 h-9 md:h-16  2xl:h-20" href="https://www.docker.com/" target="blank" rel="noreferrer"> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" /></a>
-                                <a className='w-9 md:w-16  2xl:w-20 h-9 md:h-16  2xl:h-20' href="https://www.mysql.com/" target="blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" /> </a>
-                                <a className='w-9 md:w-16  2xl:w-20 h-9 md:h-16  2xl:h-20' href="https://www.mongodb.com/" target="blank" rel="noreferrer"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" /></a>
-                                <a className='w-9 md:w-16  2xl:w-20 h-9 md:h-16  2xl:h-20' href="https://postman.com" target="blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" /> </a>
-                                <a className='w-9 md:w-16  2xl:w-20 h-9 md:h-16  2xl:h-20' href="https://www.microsoft.com/en-us/sql-server" target="blank" rel="noreferrer"> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original.svg" /></a>
-                                <a className='w-9 md:w-16  2xl:w-20 h-9 md:h-16  2xl:h-20' href="https://firebase.google.com/" target="blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" /> </a>
-                            </div>
-
-                        </Marquee>
-
-                        <Marquee autoFill={true}>
-                            <div className='flex items-center space-x-8 py-3 md:py-6  ml-6'>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://git-scm.com/" target="blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" /> </a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://getbootstrap.com" target="blank" rel="noreferrer"> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" /></a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://www.chartjs.org" target="blank" rel="noreferrer"> <img src="https://www.chartjs.org/media/logo-title.svg" alt="chartjs" /> </a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://tailwindcss.com/" target="blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" /> </a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://www.typescriptlang.org/" target="blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" /> </a>
-                                <a className='w-9 md:w-16   2xl:w-20 h-9 md:h-16   2xl:h-20' href="https://www.w3.org/html/" target="blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" /> </a>
-                            </div>
-                        </Marquee>
-                    </div>
-                </motion.section>
+            <section className='mb-24 flex md:hidden'>
+                {[stacks1, stacks2, stacks3].map((stack, index) => (
+                    <section key={index} className={`w-full flex justify-center my-4`}>
+                        <div className='flex flex-col items-center gap-3 justify-between w-[90%]'>
+                            {stack.map((item, subIndex) => (
+                                <div key={subIndex} className={`w-fit border ${subIndex%2== 0 ? "reverse-vertical-move-animation": "vertical-move-animation"} rounded-full
+                                 h-[80px] mx-2 flex justify-center aspect-square`}>
+                                    <Image src={item} width={50} height={50} className='h-auto aspect-square w-[30px]' alt="" />
+                                </div>
+                            ))}
+                        </div>
+                    </section>
+                ))}
             </section>
         </div>
 
