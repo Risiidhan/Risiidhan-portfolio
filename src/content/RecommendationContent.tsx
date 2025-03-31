32336b0fdf834803e51561a1e87af206558608ed
Risiidhan <rkishon4@gmail.com>
2025-03-31 17:22:47 +0400

    testimonial updates

--- src/content/RecommendationContent.tsx
+++ src/content/RecommendationContent.tsx
@@ -1,35 +1,52 @@
 export const recommendationsData = [
     {
-      "firstName": "Jose Albert",
-      "lastName": "Arnedo",
-      "company": "gsmfeed",
-      "jobTitle": "Web Developer",
-      "text": "I had the pleasure of working alongside Risiidhan, and I can confidently say he is an outstanding full-stack web developer and a fantastic colleague. His technical expertise spans both front-end and back-end development, and he consistently delivers clean, efficient, and scalable code. Beyond his technical skills, what truly sets Risiidhan apart is his collaborative nature and problem-solving mindset. He is always ready to help, share knowledge, and tackle challenges with a positive attitude. His ability to break down complex problems and find innovative solutions makes him a valuable asset to any team.",
-      "img": "jose.jpg"
+        "firstName": "Jose Albert",
+        "lastName": "Arnedo",
+        "company": "gsmfeed",
+        "jobTitle": "Web Developer",
+        "text": "I had the pleasure of working alongside Risiidhan, and I can confidently say he is an outstanding full-stack web developer and a fantastic colleague. His technical expertise spans both front-end and back-end development, and he consistently delivers clean, efficient, and scalable code. Beyond his technical skills, what truly sets Risiidhan apart is his collaborative nature and problem-solving mindset. He is always ready to help, share knowledge, and tackle challenges with a positive attitude. His ability to break down complex problems and find innovative solutions makes him a valuable asset to any team.",
+        "img": "jose.jpg"
     },
     {
-      "firstName": "Umar",
-      "lastName": "Uzman Faraaz",
-      "company": "gsmfeed",
-      "jobTitle": "Senior Full Stack Developer",
-      "text": "I’ve had the pleasure of working with Risiidhan at gsmfeed, and he consistently stands out for his exceptional performance and dedication. He not only delivers high quality solutions on time but also adheres to best practices, ensuring the codebase remains clean, maintainable, and easy to work with. Risiidhan’s focus on writing robust, scalable code really sets him apart, it’s clear he cares deeply about laying a strong foundation for any project he’s involved in. On top of that, his collaborative spirit makes him a true asset to any team. He’s always willing to share insights, refine existing processes, and offer support whenever needed. If you’re looking for someone who combines technical excellence, forward-thinking problem-solving, and a genuine drive to exceed expectations, Risiidhan is your guy.",
-      "img": "umar.jpg"
+        "firstName": "Umar",
+        "lastName": "Uzman Faraaz",
+        "company": "gsmfeed",
+        "jobTitle": "Senior Full Stack Developer",
+        "text": "I’ve had the pleasure of working with Risiidhan at gsmfeed, and he consistently stands out for his exceptional performance and dedication. He not only delivers high quality solutions on time but also adheres to best practices, ensuring the codebase remains clean, maintainable, and easy to work with. Risiidhan’s focus on writing robust, scalable code really sets him apart, it’s clear he cares deeply about laying a strong foundation for any project he’s involved in. On top of that, his collaborative spirit makes him a true asset to any team. He’s always willing to share insights, refine existing processes, and offer support whenever needed. If you’re looking for someone who combines technical excellence, forward-thinking problem-solving, and a genuine drive to exceed expectations, Risiidhan is your guy.",
+        "img": "umar.jpg"
     },
     {
-      "firstName": "Poornima",
-      "lastName": "Senarathna",
-      "company": "Fintechnology Asia Pacific (FINAP)",
-      "jobTitle": "Senior Business Analyst",
-      "text": "I had the pleasure of managing Rissidhan at Fintechnology Asia Pacific Lanka Private Limited, where he consistently demonstrated exceptional skills as a Software Engineer. Rissidhan was a highly productive team member who reliably met deadlines and contributed significantly to our project. What sets Rissidhan apart is not just his technical skills but also his collaborative and communicative nature. Rissidhan would be an asset to any development team.",
-      "img": "user.png"
+        "firstName": "Poornima",
+        "lastName": "Senarathna",
+        "company": "Fintechnology Asia Pacific (FINAP)",
+        "jobTitle": "Senior Business Analyst",
+        "text": "I had the pleasure of managing Rissidhan at Fintechnology Asia Pacific Lanka Private Limited, where he consistently demonstrated exceptional skills as a Software Engineer. Rissidhan was a highly productive team member who reliably met deadlines and contributed significantly to our project. What sets Rissidhan apart is not just his technical skills but also his collaborative and communicative nature. Rissidhan would be an asset to any development team.",
+        "img": "user.png"
     },
     {
-      "firstName": "Rifnaz",
-      "lastName": "Hanifa",
-      "company": "Waterlily Labs",
-      "jobTitle": "Software Engineer",
-      "text": "I highly recommend Rishiidhan for any role requiring a strong technical background and excellent communication skills. I had the pleasure of working with Rishiidhan on several undergraduate projects where he demonstrated exceptional knowledge in Angular, js, firebase, .net, asp.net, and c#. He was always a valuable contributor to the team, utilizing his problem-solving skills to help overcome any obstacles. In addition to his technical ability, Rishiidhan is a true team player who always goes the extra mile to ensure the success of the project. I am confident that Rishiidhan will be a valuable asset to any organization he joins.",
-      "img": "rifnaz.jpg"
+        "firstName": "Rifnaz",
+        "lastName": "Hanifa",
+        "company": "London Metropolitan University",
+        "jobTitle": "Software Engineer",
+        "text": "I highly recommend Rishiidhan for any role requiring a strong technical background and excellent communication skills. I had the pleasure of working with Rishiidhan on several undergraduate projects where he demonstrated exceptional knowledge in Angular, js, firebase, .net, asp.net, and c#. He was always a valuable contributor to the team, utilizing his problem-solving skills to help overcome any obstacles. In addition to his technical ability, Rishiidhan is a true team player who always goes the extra mile to ensure the success of the project. I am confident that Rishiidhan will be a valuable asset to any organization he joins.",
+        "img": "rifnaz.jpg"
+    },
+    {
+        "firstName": "Mohamed",
+        "lastName": "Ajmal",
+        "company": "London Metropolitan University",
+        "jobTitle": "Software Engineer",
+        "text": `I had the privilege of collaborating with Risiidhan throughout our degree program, and I can confidently say he is an exceptional talent. Their coding contributions consistently elevated our projects to the next level, combining technical excellence with creative problem-solving. What truly sets Risiidhan's apart is his remarkable ability to manage time effectively and coordinate tasks, he never missed a deadline and often helped organise our team's workflow to maximise efficiency. 
+Any organisation would benefit tremendously from Risiidhan's combination of coding proficiency, time management skills, and collaborative spirit. I wholeheartedly recommend him and look forward to seeing his continued success.`,
+        "img": "ajmal.jpg"
+    },
+    {
+        "firstName": "Muhammed",
+        "lastName": "Azran",
+        "company": "London Metropolitan University",
+        "jobTitle": "UI/UX Designer",
+        "text": `I had the pleasure of studying alongside Risiidhan at London Metropolitan University, where we collaborated on multiple projects that truly showcased his brilliance. As a developer, he’s phenomenal, particularly with front-end technologies like Angular, JavaScript, and TypeScript, Raact and NextJS. His skills go beyond just coding. He has an incredible ability to refine and elevate every detail, turning good ideas into polished, standout final products. He’s got a sharp eye for design and functionality, and his work consistently brings that extra layer that makes everything shine. 
+On top of that, he’s an amazing teammate with full of energy, creative insights, and a relentless drive to get things right. Anyone who works with Risiidhan will see he’s a game-changer and an invaluable part of any team. I’d recommend him without hesitation!`,
+        "img": "azran.jpg"
     }
-  ]
-  
+]
