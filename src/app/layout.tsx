52a4a670114be4c45548713d1c720f5c811a7414
Risiidhan <rkishon4@gmail.com>
2025-07-12 18:38:58 +0400

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     title: "Risiidhan Punniyamoorthy",
-    description: `Hi, I'm a Software Engineer who have demonstrated a strong ability
-                to adapt quickly and learn independently. My journey began with a
-                role where I served to enhance website responsiveness 
-                and now I play a full stack role in the development of comprehensive projects`,
+    description: `Hi, I'm a Software Engineer who have demonstrated a strong ability to adapt quickly and learn independently. My journey began with a role where I served to enhance website responsiveness and now I play a full stack role in the development of comprehensi  ve projects`,
     images: [
       {
         url: "https://media.licdn.com/dms/image/v2/D5603AQG2Ag0pXiq8bw/profile-displayphoto-shrink_800_800/B56ZXCA5DIHQAc-/0/1742716770343?e=1748476800&v=beta&t=46djyk2paClwkeu8SBVWTenpfvmUHJDXo61H7_t9ZA4",
