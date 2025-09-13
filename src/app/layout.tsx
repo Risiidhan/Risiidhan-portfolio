fde35ad5e6d769bd943c07f0804db78ab85ab848
Risiidhan <rkishon4@gmail.com>
2025-09-13 17:14:59 +0400

    add: profile-landscape

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export const metadata: Metadata = {
     locale: "en_US",
     images: [
       {
-        url: "https://risiidhan.com/profile-pic.png",
-        width: 1200,
-        height: 630,
+        url: "https://risiidhan.com/profile-landscape.png",
+        width: 400,
+        height: 400,
         alt: "Risiidhan",
       },
     ],
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     description: `Hi, I'm a Software Engineer who have demonstrated a strong ability to adapt quickly and learn independently. My journey began with a role where I served to enhance website responsiveness and now I play a full stack role in the development of comprehensi  ve projects`,
     images: [
       {
-        url: "https://risiidhan.com/profile-pic.png",
+        url: "https://risiidhan.com/profile-landscape.png",
         width: 400,
         height: 400,
         alt: "Risiidhan",
