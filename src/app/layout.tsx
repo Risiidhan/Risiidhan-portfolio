2fa7cef1814f9fb2545fd7024092542145a66396
Risiidhan <rkishon4@gmail.com>
2025-09-13 16:42:55 +0400

    fix: new profile pic and set to meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  authors: [{ name: "Risiidhan", url: "https://risiidhan.com/" }], 
+  metadataBase: new URL("https://risiidhan.com"),
+  authors: [{ name: "Risiidhan", url: "https://risiidhan.com/" }],
   title: "Risiidhan Punniyamoorthy - Portfolio",
   description: `Hi, I'm a Software Engineer who have demonstrated a strong ability
                 to adapt quickly and learn independently. My journey began with a
@@ -19,30 +20,30 @@ export const metadata: Metadata = {
     locale: "en_US",
     images: [
       {
-        url: "https://media.licdn.com/dms/image/v2/D5603AQG2Ag0pXiq8bw/profile-displayphoto-shrink_800_800/B56ZXCA5DIHQAc-/0/1742716770343?e=1748476800&v=beta&t=46djyk2paClwkeu8SBVWTenpfvmUHJDXo61H7_t9ZA4",
+        url: "/profile-pic.png",
         width: 400,
         height: 400,
-        alt: "Risiidhan"
+        alt: "Risiidhan",
       },
-    ]
+    ],
   },
   twitter: {
     title: "Risiidhan Punniyamoorthy",
     description: `Hi, I'm a Software Engineer who have demonstrated a strong ability to adapt quickly and learn independently. My journey began with a role where I served to enhance website responsiveness and now I play a full stack role in the development of comprehensi  ve projects`,
     images: [
       {
-        url: "https://media.licdn.com/dms/image/v2/D5603AQG2Ag0pXiq8bw/profile-displayphoto-shrink_800_800/B56ZXCA5DIHQAc-/0/1742716770343?e=1748476800&v=beta&t=46djyk2paClwkeu8SBVWTenpfvmUHJDXo61H7_t9ZA4",
+        url: "/profile-pic.png",
         width: 400,
         height: 400,
-        alt: "Risiidhan"
+        alt: "Risiidhan",
       },
-    ]
+    ],
   },
   keywords: ["Nextjs", "web developer", "software engineer"],
-icons: {
-  icon: "/favicon.ico",
-  shortcut: "/favicon.ico",
-}
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -52,11 +53,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={""}
-      >
-        {children}
-      </body>
+      <body className={""}>{children}</body>
     </html>
   );
 }
